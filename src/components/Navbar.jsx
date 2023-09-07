eac7f37ac344810f438249ac6ba0c64b10164bba
Shubhankar Valimbe <shubhankarvalimbe@gmail.com>
2023-09-07 11:03:46 -0500

    added github link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
-import './Navbar.css'
- 
+
+import "./Navbar.css";
+
 export default function Navbar() {
   return (
     <header>
-      <Link to='/' className='logo'>Quizzer</Link>
-    <nav>
-        <Link to='/'><FontAwesomeIcon icon={faHouse} /></Link>
-    </nav>
+      <Link to="/" className="logo">
+        Quizzer
+      </Link>
+      <nav>
+        <Link to="/">
+          <FontAwesomeIcon icon={faHouse} />
+        </Link>
+        <Link
+          to="https://github.com/shubhankarval/Quizzer"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faGithub} />
+        </Link>
+      </nav>
     </header>
-  )
+  );
 }
-
